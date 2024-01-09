1ffcdc8262ed2291cb20b7072eb4332678ba186c
hariomsingh945 <hariomsingh945@gmail.com>
2024-01-09 20:56:41 +0530

    delete repo

--- ResourceGroup/main.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "rg_example" {
-  for_each = var.rg_group
-  name     = each.value.name
-  location = each.value.location
-}
--- ResourceGroup/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-rg_group = {
-  "rg1" = {
-    "name"     = "rg_group1"
-    "location" = "West Europe"
-  }
-}
